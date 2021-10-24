c43bb14385e823bf3210c6cc456ff74f097f9a32
fvallada <florencia.valladares@sansano.usm.cl>
2021-10-24 05:49:12 -0300

    etapa 2 y 3 sin probar

--- Lider/server/server.go
+++ Lider/server/server.go
@@ -129,48 +129,110 @@ func (*server) Iniciar(ctx context.Context, in *api.Signal) (*api.Signal, error)
 }
 
 func (*server) Jugar(ctx context.Context, in *api.Jugadas) (*api.EstadoJugador, error) {
-	players := canPlay()
 	moves := in.Plays
-	if rnd_actual < 3 && len(players) > 0 {
-		s1 := rand.NewSource(time.Now().UnixNano())
-		r1 := rand.New(s1)
-		leaderMove := r1.Int31n(int32(4)) + int32(6)
-		//log.Printf("Lider: %v", leaderMove)
-		for _, player := range players {
-			if moves[player] >= leaderMove {
-				est_jugadores[player] = 0 //muerto
-				//log.Printf("Jugador %v ha muerto, tiro %v y tiene %v puntos", player, moves[player], pts_jugadores_e1[player])
-				log.Printf("Jugador %v ha muerto", player)
-			} else {
+	if in.Etapa == 1 {
+		players := canPlayPhase1()
+		if rnd_actual < 3 && len(players) > 0 {
+			rand.Seed(time.Now().UnixNano())
+
+			leaderMove := rand.Int31n(int32(4)) + int32(6)
+			//log.Printf("Lider: %v", leaderMove)
+			for _, player := range players {
+				if moves[player] >= leaderMove {
+					est_jugadores[player] = 0 //muerto
+					//log.Printf("Jugador %v ha muerto, tiro %v y tiene %v puntos", player, moves[player], pts_jugadores_e1[player])
+					log.Printf("Jugador %v ha muerto", player)
+				} else {
+					pts_jugadores_e1[player] = pts_jugadores_e1[player] + moves[player]
+					if pts_jugadores_e1[player] >= 21 {
+						ganadores_e1[player] = 1
+					}
+				}
+			}
+			rnd_actual = rnd_actual + 1
+			return &api.EstadoJugador{Estado: est_jugadores, Ronda: rnd_actual, JugadorGano: ganadores_e1[0]}, nil
+		} else {
+			for _, player := range players {
 				pts_jugadores_e1[player] = pts_jugadores_e1[player] + moves[player]
-				if pts_jugadores_e1[player] >= 21 {
+				if pts_jugadores_e1[player] < 21 {
+					est_jugadores[player] = 0
+					//log.Printf("Jugador %v ha muerto, tiro %v y tiene %v puntos", player, moves[player], pts_jugadores_e1[player])
+					log.Printf("Jugador %v ha muerto", player)
+				} else {
 					ganadores_e1[player] = 1
 				}
 			}
+			rnd_actual = 0
+			etapa_actual = 2
+			fmt.Print("Jugadores vivos: ")
+			for i := range est_jugadores {
+				if est_jugadores[i] == 1 {
+					fmt.Printf("%v ", i)
+				}
+			}
+			fmt.Println()
+			return &api.EstadoJugador{Estado: est_jugadores, Ronda: 4, JugadorGano: ganadores_e1[0]}, nil
 		}
-		rnd_actual = rnd_actual + 1
-		return &api.EstadoJugador{Estado: est_jugadores, Ronda: rnd_actual, JugadorGano: ganadores_e1[0]}, nil
-	} else {
-		for _, player := range players {
-			pts_jugadores_e1[player] = pts_jugadores_e1[player] + moves[player]
-			if pts_jugadores_e1[player] < 21 {
-				est_jugadores[player] = 0
-				//log.Printf("Jugador %v ha muerto, tiro %v y tiene %v puntos", player, moves[player], pts_jugadores_e1[player])
-				log.Printf("Jugador %v ha muerto", player)
+	} else if in.Etapa == 2 {
+		rand.Seed(time.Now().UnixNano())
+		leaderMove := rand.Int31n(int32(3)) + int32(1)
+		players := canPlayPhase2()
+		if len(players)%2 == 1 { //es impar
+			indexToDelete := rand.Int31n(int32(len(players))) + int32(1)
+			est_jugadores[indexToDelete] = 0 //muerto
+			log.Printf("Jugador %v ha muerto", players[indexToDelete])
+			players = append(players[:indexToDelete], players[indexToDelete+1:]...)
+		}
+		teamA := players[0 : len(players)/2]
+		teamB := players[len(players)/2:]
+
+		if sum(teamA)%2 == sum(teamB)%2 && sum(teamA)%2 != leaderMove%2 {
+			if rand.Int31n(int32(1)) == 0 {
+				for _, player := range teamA {
+					est_jugadores[player] = 0
+					log.Printf("Jugador %v ha muerto", player)
+				}
 			} else {
-				ganadores_e1[player] = 1
+				for _, player := range teamB {
+					est_jugadores[player] = 0
+					log.Printf("Jugador %v ha muerto", player)
+				}
+			}
+		} else if sum(teamA)%2 != leaderMove%2 {
+			for _, player := range teamA {
+				est_jugadores[player] = 0
+				log.Printf("Jugador %v ha muerto", player)
+			}
+		} else if sum(teamB)%2 != leaderMove%2 {
+			for _, player := range teamB {
+				est_jugadores[player] = 0
+				log.Printf("Jugador %v ha muerto", player)
 			}
 		}
-		rnd_actual = 0
-		etapa_actual = 2
-		fmt.Print("Jugadores vivos: ")
-		for i := range est_jugadores {
-			if est_jugadores[i] == 1 {
-				fmt.Printf("%v ", i)
+		return &api.EstadoJugador{Estado: est_jugadores}, nil
+	} else {
+		rand.Seed(time.Now().UnixNano())
+		leaderMove := rand.Int31n(int32(3)) + int32(1)
+		players := canPlayPhase2()
+		if len(canPlayPhase2())%2 == 1 { //es impar
+			indexToDelete := rand.Int31n(int32(len(players))) + int32(1)
+			est_jugadores[indexToDelete] = 0 //muerto
+			log.Printf("Jugador %v ha muerto", players[indexToDelete])
+			players = append(players[:indexToDelete], players[indexToDelete+1:]...)
+		}
+		playerCouples := tuples(players)
+		for _, couple := range playerCouples {
+			if couple[0] != couple[1] {
+				if Abs(couple[0]-leaderMove) > Abs(couple[1]-leaderMove) {
+					est_jugadores[couple[0]] = 0
+					log.Printf("Jugador %v ha muerto", couple[0])
+				} else {
+					est_jugadores[couple[1]] = 0
+					log.Printf("Jugador %v ha muerto", couple[1])
+				}
 			}
 		}
-		fmt.Println()
-		return &api.EstadoJugador{Estado: est_jugadores, Ronda: 4, JugadorGano: ganadores_e1[0]}, nil
+		return &api.EstadoJugador{Estado: est_jugadores}, nil
 	}
 }
 
@@ -181,7 +243,7 @@ func (*server) Monto(ctx context.Context, in *api.PedirMonto) (*api.MontoJugador
 
 */
 
-func canPlay() []int32 {
+func canPlayPhase1() []int32 {
 	var result []int32
 	for i := range est_jugadores {
 		// si esta vivo y no ha ganado la etapa
@@ -190,4 +252,40 @@ func canPlay() []int32 {
 		}
 	}
 	return result
+}
+
+func canPlayPhase2() []int32 {
+	var result []int32
+	for i := range est_jugadores {
+		// si esta vivo y no ha ganado la etapa
+		if est_jugadores[i] == 1 {
+			result = append(result, int32(i))
+		}
+	}
+	return result
+}
+
+func sum(array []int32) int32 {
+	result := int32(0)
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
+func tuples(array []int32) [][]int32 {
+	var result [][]int32
+	index := 0
+	for index < len(array) {
+		result = append(result, array[index:index+2])
+		index = index + 2
+	}
+	return result
+}
+
+func Abs(x int32) int32 {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
