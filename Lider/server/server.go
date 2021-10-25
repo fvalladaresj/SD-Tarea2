a46306685ec23192838623ba795306dddd718a85
fvallada <florencia.valladares@sansano.usm.cl>
2021-10-25 02:22:56 -0300

    rabbitMQ implementado

--- Lider/server/server.go
+++ Lider/server/server.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"strconv"
 	"time"
 
 	"github.com/fvalladaresj/SD-Tarea2/Lider/api"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
 
@@ -142,6 +144,7 @@ func (*server) Jugar(ctx context.Context, in *api.Jugadas) (*api.EstadoJugador, 
 					est_jugadores[player] = 0 //muerto
 					//log.Printf("Jugador %v ha muerto, tiro %v y tiene %v puntos", player, moves[player], pts_jugadores_e1[player])
 					log.Printf("Jugador %v ha muerto", player)
+					go sendRabbit(player, etapa_actual)
 				} else {
 					pts_jugadores_e1[player] = pts_jugadores_e1[player] + moves[player]
 					if pts_jugadores_e1[player] >= 21 {
@@ -158,13 +161,14 @@ func (*server) Jugar(ctx context.Context, in *api.Jugadas) (*api.EstadoJugador, 
 					est_jugadores[player] = 0
 					//log.Printf("Jugador %v ha muerto, tiro %v y tiene %v puntos", player, moves[player], pts_jugadores_e1[player])
 					log.Printf("Jugador %v ha muerto", player)
+					go sendRabbit(player, etapa_actual)
 				} else {
 					ganadores_e1[player] = 1
 				}
 			}
 			rnd_actual = 0
 			etapa_actual = 2
-			fmt.Print("Jugadores vivos: ")
+			fmt.Print("Etapa 1 finalizada, jugadores vivos: ")
 			for i := range est_jugadores {
 				if est_jugadores[i] == 1 {
 					fmt.Printf("%v ", i)
@@ -288,4 +292,42 @@ func Abs(x int32) int32 {
 		return -x
 	}
 	return x
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func sendRabbit(player int32, round int32) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"Jugadores muertos", // name
+		false,               // durable
+		false,               // delete when unused
+		false,               // exclusive
+		false,               // no-wait
+		nil,                 // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Jugador_" + strconv.FormatInt(int64(player), 10) + " " + "Ronda_" + strconv.FormatInt(int64(round), 10)
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 }
